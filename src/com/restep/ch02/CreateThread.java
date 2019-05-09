f8c888576ad3f9f0bf92db6ca1f1ef5ed627efd9
restep <restep@163.com>
2019-05-09 11:19:26 +0800

    多线程与JVM

--- src/com/restep/ch02/CreateThread.java
+++ src/com/restep/ch02/CreateThread.java
@@ -2,6 +2,7 @@ package com.restep.ch02;
 
 /**
  * 分析创建线程
+ *
  * @author restep
  * @date 2019/5/9
  */
@@ -15,5 +16,22 @@ public class CreateThread {
 
         System.out.println(thread01.getName());
         System.out.println(thread02.getName());
+
+        Thread thread03 = new Thread("myThreadFirst");
+        Thread thread04 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("thread04");
+            }
+        });
+
+        //传递了名字 JVM就不会再生成Thread-0 Thread-1这样的默认名称
+        System.out.println(thread03.getName());
+        System.out.println(thread04.getName());
+
+        Thread thread05 = new Thread(() -> {
+            System.out.println("thread05");
+        }, "myThreadSecond");
+        System.out.println(thread05.getName());
     }
 }
---
+++ src/com/restep/ch02/ThreadJvm.java
@@ -0,0 +1,42 @@
+package com.restep.ch02;
+
+/**
+ * 线程内存分析
+ * Thread(ThreadGroup group, Runnable target, String name, long stackSize)
+ *
+ * @author restep
+ * @date 2019/5/9
+ */
+public class ThreadJvm {
+    private int i = 0;
+    private byte[] byteArr = new byte[1024];
+    private static int counter = 0;
+
+    /**
+     * 创建虚拟机栈
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        //局部变量表
+        int j = 0;
+        //局部变量表存放引用地址  数组存放在堆中
+        int[] arr = new int[1024];
+
+        /**
+         * 虚拟机栈
+         * java.lang.StackOverflowError
+         */
+        try {
+            add(0);
+        } catch (Error e) {
+            e.printStackTrace();
+            System.out.println(counter);
+        }
+    }
+
+    private static void add(int i) {
+        ++counter;
+        add(i + 1);
+    }
+}
