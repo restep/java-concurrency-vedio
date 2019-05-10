601e66228aa15083dca2568481abe041abe606cc
restep <restep@163.com>
2019-05-10 11:38:40 +0800

    死锁

---
+++ src/com/restep/ch04/DeadLockTest.java
@@ -0,0 +1,33 @@
+package com.restep.ch04;
+
+/**
+ * @author restep
+ * @date 2019/5/10
+ */
+public class DeadLockTest {
+    public static void main(String[] args) {
+        OtherService otherService = new OtherService();
+        DeadLock deadLock = new DeadLock(otherService);
+        otherService.setDeadLock(deadLock);
+
+        new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    deadLock.m1();
+                }
+            }
+        }.start();
+
+        new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    while (true) {
+                        otherService.s2();
+                    }
+                }
+            }
+        }.start();
+    }
+}
